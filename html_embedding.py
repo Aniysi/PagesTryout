5c19804c011638e0f566de4044d822a8e64d637f
Leo <leonardo.trolese@studenti.unipd.it>
2024-11-21 21:22:40 +0100

    Miglioramenti salvataggio dei log

--- html_embedding.py
+++ html_embedding.py
@@ -6,12 +6,18 @@ from pathlib import Path
 # Dato il path di un file lo compila in .pdf mediante il comando 'pdflatex'
 def compile(dir_path, file_path):
     try:
-        with open(f"compilation.log", "w") as log_file:
+        with open(f"compilation.log", "a") as log_file:
+            log_file.write("------------------------------------------------------------------------------------------------------")
             subprocess.run(["pdflatex", file_path], cwd=dir_path, check=True, stdout=log_file, stderr=log_file)
         #print(f"{file_path} compilato con successo!")
     except subprocess.CalledProcessError:
-        with open("info.log", "w") as test:
+        with open(f"errors.log", "w") as test:
             test.write(f"Errore nella compilazione di {file_path}!")
+
+
+# Cancellazione log di compilazione e log degli errori
+os.remove("compilation.log")
+os.remove("errors.log")
 
 # Vista riscorsiva della cartella corrente per individuare ogni file .tex
 for cartella, sottocartelle, files in os.walk(os.getcwd()):
